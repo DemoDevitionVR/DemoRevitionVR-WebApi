a1f76a84bbf55270f00eafbe47910827998daacc
Nurullo <ghostone234qwer@gmail.com>
2023-10-12 15:42:54 +0500

    Added DTOs

---
+++ src/Demo.Revition.Service/DTOs/Devices/DeviceResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Demo.Revition.Service.DTOs.Devices;
+
+public class DeviceResultDto
+{
+    public long Id { get; set; }
+    public long DeviceId { get; set; }
+    public string Name { get; set; }
+}
